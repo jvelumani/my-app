527a85c5d8413e49a48a11afe6fd41af482e85c9
velumani <j.velumani@gmail.com>
2018-07-13 16:45:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1> Movie Store</h1>
+        </header>
+        <section>
+        <p className="App-intro">
+        
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        </section>
+       
+        <button className="btn btn-primary">Submit</button>
+      </div>
+    );
+  }
+}
+
+export default App;
