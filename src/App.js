46c6b50a9bb1c04e4d16a6325659620d5f12b397
velumani <j.velumani@gmail.com>
2018-07-13 17:04:58 +0530

    But
    Button

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ class App extends Component {
         </header>
         <section>
         <p className="App-intro">
-        
+
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
         </section>
        
-        <button className="btn btn-primary">Submit</button>
+        <button className="btn btn-primary">Submit Here</button>
       </div>
     );
   }
