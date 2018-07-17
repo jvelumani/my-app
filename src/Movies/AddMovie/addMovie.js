78aad693c8c2fc7f1f14a0e95599e9ca876a162e
sridhand <sridhand@in.ibm.com>
2018-07-17 17:56:52 +0530

    Added buttons

---
+++ src/Movies/AddMovie/addMovie.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AddMovies extends React.Component{
+
+    render(){
+        return(
+            <div className="addMovie movieActionButtons">
+				<button className="primary">Add Movie</button>
+            </div>
+        
+        )
+    }
+}
+export default AddMovies;
+
---
+++ src/Movies/LoadMovie/moreMovie.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MoreMovies extends React.Component{
+
+    render(){
+        return(
+            <div className="movieMore movieActionButtons">
+				<button className="primary">More Movies</button>
+            </div>
+        
+        )
+    }
+}
+export default MoreMovies;
+
