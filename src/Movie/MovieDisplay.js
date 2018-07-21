6ead2c4e9c9081bdc38462b92cf0e49f06e36994
Alikhan <muhammedalikhan27@gmail.com>
2018-07-21 11:41:03 +0530

    implemented 'More Movies' as seperate a component

--- src/Movie/MovieDisplay.js
+++ src/Movie/MovieDisplay.js
@@ -1,102 +1,111 @@
-import React, { Component } from 'react'
-
-class MovieDisplay extends Component {
-
-  constructor() {
-    super()
+import React from "react";
+class MovieDisplay extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
       videos: [
-
         {
           id: 1,
           name: "kabali",
-          movieImage: '../images/kabali.png',
+          movieImage: "../images/kabali.png",
           movieYear: 2009
-
         },
         {
           id: 2,
           name: "kabali2",
-          movieImage: '../images/kabali.png',
+          movieImage: "../images/kabali.png",
           movieYear: 2009
-
         },
         {
           id: 3,
           name: "kabali3",
-          movieImage: '../images/kabali.png',
+          movieImage: "../images/kabali.png",
           movieYear: 2009
-
         },
         {
           id: 4,
           name: "Captain",
-          movieImage: '../images/kabali.png',
+          movieImage: "../images/kabali.png",
           movieYear: 2009
-
         },
         {
-          id: 4,
+          id: 5,
           name: "Captain",
-          movieImage: '../images/kabali.png',
+          movieImage: "../images/kabali.png",
           movieYear: 2009
-
         },
         {
-          id: 4,
+          id: 6,
           name: "Captain",
-          movieImage: '../images/kabali.png',
+          movieImage: "../images/kabali.png",
           movieYear: 2009
-
+        },
+        {
+          id: 7,
+          name: "Captain",
+          movieImage: "../images/kabali.png",
+          movieYear: 2009
         }
-        , {
-          id: 4,
-          name: "Captain",
-          movieImage: '../images/kabali.png',
-          movieYear: 2009
-
-        }
-      ], isItemContentVisible :{},
-      item:[]
-
-    }
-
-
+      ],
+      isItemContentVisible: {},
+      item: [],
+      size: 3
+    };
   }
-
-showContent(id) {
-  // merge new value with existing visibility status into new object
-  this.setState({
-    isItemContentVisible: {
-      ...this.state.isItemContentVisible,
-      [id]: true
-    }
-  });
-}
-
+  handleSize = () => {
+    this.setState({
+      size: this.state.size + 3
+    });
+  };
   render() {
-
-    let size = 3;
-    const alubmItems = this.state.videos.slice(0, size).map((post, index) => (
-
+    let items = this.state.videos.slice(0, this.state.size);
+    const alubmItems = items.map((post, index) => (
       <div className="col-md-2 movie-item" key={post.id}>
         <h4>{post.name}</h4>
-        <img src={post.movieImage} alt=""/>
+        <img src={post.movieImage} alt="" />
         <span>{post.movieYear}</span>
       </div>
-
-    ))
+    ));
     return (
-
       <div>
         <p>here is the move display</p>
         {alubmItems}
-       <div>
-           <button  onClick={() => this.showContent()} className="btn btn-success">More Movies</button>
+        <div>
+          <Button
+            size={this.state.size}
+            len={this.state.videos.length}
+            incrementSize={this.handleSize}
+          />
         </div>
       </div>
-    )
+    );
   }
 }
-
-export default MovieDisplay
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      disabled: false
+    };
+  }
+  showContent() {
+    this.props.incrementSize();
+    if (this.props.len <= this.props.size) {
+      this.setState({ disabled: true });
+    } else {
+      this.setState({ disabled: false });
+    }
+  }
+  render() {
+    return (
+      <button
+        disabled={this.state.disabled}
+        onClick={() => this.showContent()}
+        className="btn btn-success"
+      >
+        More Movies
+      </button>
+    );
+  }
+}
+export default MovieDisplay;
